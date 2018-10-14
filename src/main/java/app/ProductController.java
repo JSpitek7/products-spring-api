debc6da69c6a87b7a1d3142215b82002684ad8b5
JSpitek7 <jspitek7@vt.edu>
2018-10-14 01:56:01 -0400

    added endpoints for getting categories list and suppliers list

--- src/main/java/app/ProductController.java
+++ src/main/java/app/ProductController.java
@@ -3,6 +3,7 @@ package app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ProductController {
     @Autowired ProductRepository productRepository;
@@ -13,18 +14,28 @@ public class ProductController {
     }
 //
     @GetMapping("/products/category/{category}")
-    public Iterable<Product> getProductsByCategory(@PathVariable String category) {
+    public @ResponseBody Iterable<Product> getProductsByCategory(@PathVariable String category) {
         return productRepository.findByCategory(category);
     }
 
     @GetMapping("products/supplier/{supplier}")
-    public Iterable<Product> getProductsBySupplier(@PathVariable String supplier) {
+    public @ResponseBody Iterable<Product> getProductsBySupplier(@PathVariable String supplier) {
         return productRepository.findBySupplier(supplier);
     }
 
     @GetMapping("products/{category}/{supplier}")
-    public Iterable<Product> getProductsByCategoryAndSupplier(@PathVariable String category,
+    public @ResponseBody Iterable<Product> getProductsByCategoryAndSupplier(@PathVariable String category,
                                                               @PathVariable String supplier) {
         return productRepository.findByCategoryAndSupplier(category,supplier);
+    }
+
+    @GetMapping("/categories")
+    public @ResponseBody Iterable<String> getCategories() {
+        return productRepository.findDistinctCategory();
+    }
+
+    @GetMapping("/suppliers")
+    public @ResponseBody Iterable<String> getSuppliers() {
+        return productRepository.findDistinctSupplier();
     }
 }
--- src/main/java/app/ProductRepository.java
+++ src/main/java/app/ProductRepository.java
@@ -1,5 +1,6 @@
 package app;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -8,4 +9,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     Iterable<Product> findByCategory(String category);
     Iterable<Product> findBySupplier(String supplier);
     Iterable<Product> findByCategoryAndSupplier(String category, String supplier);
+    @Query("select distinct u.category from #{#entityName} u")
+    Iterable<String> findDistinctCategory();
+    @Query("select distinct u.supplier from #{#entityName} u")
+    Iterable<String> findDistinctSupplier();
 }
